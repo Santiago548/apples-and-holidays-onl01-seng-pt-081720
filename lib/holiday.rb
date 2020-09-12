40f748b33da1bd770c5fd61682f25a658888a7c4
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-12 00:41:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,8 @@ end
   end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-
+  holiday_hash[season][holiday_name] = supply_array
+   holiday_hash
 end
 
   # code here
