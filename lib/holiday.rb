4d7c8266ef4d6ee224bdc822dd63f15f51fe676f
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-12 00:24:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,12 +23,9 @@ end
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-def add_supply_to_winter_holidays(holiday_hash)
-  holiday_hash.each do |holiday,supply|
-    if holiday == winter << supply
-  end
-end
-end
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
